b603ecbb31e97cd946d2e1a6fc71a5b9e46f7720
Gilberto Antonio Rodríguez Zetino <gilberto.rodriguez@dcegisa.local>
2024-05-27 22:26:01 -0600

    tipicos

---
+++ apptp.js
@@ -0,0 +1,26 @@
+let items=document.querySelector(' .slider .item2 ');
+let next=document.getElementById('next')
+let prev=document.getElementById('prev')
+let thumbnails=document.querySelectorAll('.thumbail .item2')
+
+
+
+let countItem=items.length;
+let itemActive=0;
+
+
+next.onclick= function(){
+    itemActive=itemActive +1;
+
+    if(itemActive>=countItem){
+        itemActive=0;
+    }
+
+    showSlider();
+}
+
+function showSlider(){
+    let previewItemActived=document.querySelector('.slider .List .item.active')
+    let thumbailActiveOld=document.querySelector()
+    previewItemActived.classList.remove('active')
+}
